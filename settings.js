c9759eb118c2ff5aa02da84d1f7154dc412437bc
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-06 12:06:28 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.packname = 'ArullBotzMD'//BEBAS
 global.author = 'ArullOfc'
 
 // Thumb & Image random
-global.thumb = fs.readFileSync('./image/kiki.jpg')// jangan di ubah nanti eror
+global.thumb = fs.readFileSync('./image/kanna.jpg')// jangan di ubah nanti eror
 
 /*
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
